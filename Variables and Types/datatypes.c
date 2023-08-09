c1dfa7ebd21053a76797e9ac19cbaab483e3dd73
Aryan <aryannnaik@outlook.com>
2023-08-09 17:38:58 -0400

    datatypes

--- Variables and Types/datatypes.c
+++ Variables and Types/datatypes.c
@@ -2,13 +2,16 @@
 
 int main()
 {
-    int a = 3;
-    float b = 4.5;
-    double c = 5.25;
-    float sum;
+    /* TODO: define the grades variable here */
+    int grades[3];
+    int average;
 
-    sum = a + b + c;
-
-    printf("The sum of a, b, and c is %f.", sum);
+    grades[0] = 80;
+    /* TODO: define the missing grade
+       so that the average will sum to 85. */
+    grades[1] = 85;
+    grades[2] = 90;
+    average = (grades[0] + grades[1] + grades[2]) / 3;
+    printf("The average of the 3 grades is: %d", average);
     return 0;
 }
