c46077431dd391e5eee66a461c5e6ab382b7850e
Aryan <aryannnaik@outlook.com>
2023-08-09 17:38:51 -0400

    arrays

---
+++ Arrays/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // declare and initialize an array of 10 ints
+    int numbers[10];
+
+    numbers[0] = 1;
+    numbers[2] = 2;
+
+    printf("%d\n", numbers[0]);
+
+    return 0;
+}
